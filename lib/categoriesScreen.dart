877d4732f5dc25bf0ba44ca86631d53abe504569
Vishnu <vishnupothan2@gmail.com>
2020-11-01 10:08:48 +0530

    theme added

---
+++ lib/categoriesScreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import './dummyData.dart';
+import 'categoryItem.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recipe'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
